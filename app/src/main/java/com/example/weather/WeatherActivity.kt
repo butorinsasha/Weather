c4d466be2c44da6a8183e931e08d0eed30b2151a
a.b.butorin <a.b.butorin@tinkoff.ru>
2024-04-24 16:25:05 +0300

    Firebase analytics and messaging

--- app/src/main/java/com/example/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/weather/WeatherActivity.kt
@@ -2,16 +2,33 @@ package com.example.weather
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
+
 
 class WeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseApp.initializeApp(this)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("TAG", "Fetching FCM registration token failed", task.exception)
+            }
+
+            val fcmRegistrationToken = task.result
+
+            Log.i("TAG", "FCM token = $fcmRegistrationToken")
+        }
+
         setContentView(R.layout.activity_weather)
 
         val weatherListView = findViewById<ListView>(R.id.weather_list)
